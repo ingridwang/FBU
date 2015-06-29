a9e83f940405eb3ad5e03b137e27586df85cbb9f
Rebecca Chen <chen676@usc.edu>
2015-06-29 14:46:27 -0700

    setting up controller classes

---
+++ game/game/PlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayViewController.h
+//  game
+//
+//  Created by Rebecca Chen on 6/29/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayViewController : UIViewController
+
+@end
