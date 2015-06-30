2a1d1edb5db7eb30b82c03cd108124485846b59d
Rebecca Chen <chen676@usc.edu>
2015-06-30 13:26:12 -0700

    adding game attempt

---
+++ game/game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  game
+//
+//  Created by Rebecca Chen on 6/29/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ game/game/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  game
+//
+//  Created by Rebecca Chen on 6/29/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+//property for score, level, current speed
+@end
+
