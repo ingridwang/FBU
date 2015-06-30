3c082bb6fd775119c6d5bebf8b8bb951cb7c64e4
Ingrid Wang <ingridwa@>
2015-06-29 18:00:17 -0700

    Level label added and fixed

--- game/game/Game.h
+++ game/game/Game.h
@@ -6,12 +6,12 @@
 //  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface Game : NSObject
 @property(nonatomic)BOOL timerOn;
 @property(nonatomic)BOOL speedUp;
-@property(nonatomic, weak) UIImage* image;
+@property(nonatomic, weak) UIImage *image;
 @property(nonatomic)int currentLevel;
 
 
